28dec3a00cb4f97122f0386aa527c4a90148d112
MrZ-0714 <Z@gmail.com>
2021-01-23 17:12:17 -0800

    Add input component and it's animation.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/header.compenent";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./sign-in-and-sign-up.styles.scss";
+
+import SignIn from "../../components/sign-in/sign-in.compenent";
+
+const SignInAndSignUpPage = () => (
+  <div className="sign-sign-in-and-sign-up">
+    SIGN IN
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUpPage;
