4dcf9f16bc40a1c3ebc11f3be9f057cff00c22a7
MrZ-0714 <Z@gmail.com>
2021-01-24 23:09:02 -0800

    test

--- src/components/sign-in/sign-in.compenent.jsx
+++ src/components/sign-in/sign-in.compenent.jsx
@@ -30,7 +30,7 @@ class SignIn extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="sign-in">
         <h2>I already have account </h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handleSubmit}>
