efeb61e5c94cc300ade0fd07e05e65a476e5ddff
MrZ-0714 <Z@gmail.com>
2021-01-25 14:38:29 -0800

    Set up firebase for Google Auth

---
+++ src/firebase/firebaseconfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA36eyKq5fDxzqoydGqG1h19nW0-kHVJ8Q",
+  authDomain: "crwn-db-2edac.firebaseapp.com",
+  projectId: "crwn-db-2edac",
+  storageBucket: "crwn-db-2edac.appspot.com",
+  messagingSenderId: "131433880794",
+  appId: "1:131433880794:web:3a72a4773fb011dfefd9e3",
+  measurementId: "G-4CN8W2PCL2",
+};
+
+export default firebaseConfig;
