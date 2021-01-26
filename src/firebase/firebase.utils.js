648aaf0cec423e5888710808db70d12c15e140de
MrZ-0714 <Z@gmail.com>
2021-01-25 18:31:34 -0800

    Add firebase db for user info storage

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,32 @@ import "firebase/firestore";
 
 import firebaseConfig from "./firebaseconfig.js";
 // Initialize Firebase
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (err) {
+      console.log("Error creating user" + err.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
