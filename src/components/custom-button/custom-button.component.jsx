9a31881bdc5a5febe8c232d9fdb604281fa62330
MrZ-0714 <Z@gmail.com>
2021-01-25 17:27:16 -0800

    Add signin and sign out using firebase, style adjustment

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./custom-button.styles.scss";
 
-const CustomButton = ({ children, ...otherCustomButtonProps }) => (
-  <button className="custom-button" {...otherCustomButtonProps}>
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  ...otherCustomButtonProps
+}) => (
+  <button
+    className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
+    {...otherCustomButtonProps}
+  >
     {children}
   </button>
 );
