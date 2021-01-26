7104e6d1cb0c0006f5e68de90b16bcb7d92fb47c
MrZ-0714 <Z@gmail.com>
2021-01-26 02:34:39 -0800

    Add signup with email and password. BUG---- firestore is not saving new user

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ class App extends React.Component {
               ...snapShot.data(),
             },
           });
+          console.log(this.state);
         });
       } else {
         this.setState({ currentUser: userAuth });
@@ -46,8 +47,8 @@ class App extends React.Component {
         <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/signin" component={SignInAndSignUpPage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
         </Switch>
       </div>
     );
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import "./sign-in-and-sign-up.styles.scss";
 
 import SignIn from "../../components/sign-in/sign-in.compenent";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const SignInAndSignUpPage = () => (
-  <div className="sign-sign-in-and-sign-up">
-    SIGN IN
+  <div className="sign-in-and-sign-up">
     <SignIn />
+    <SignUp />
   </div>
 );
 
