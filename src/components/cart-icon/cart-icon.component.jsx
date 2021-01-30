7806ad0c3d8bfba57cc0e47bf5c477de683a81b6
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-01-29 16:26:56 -0800

    Update with reselect, replaced cart selectors with momoized selectors to improve performance

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,16 +4,22 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 //config redux for cart-dropdown hidden status.
 import { connect } from "react-redux";
 import toggleCartHiddenAction from "../../redux/cart/cart.action";
+//Selector
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors.js";
 
-const CartIcon = ({ toogleCartHiddenProps }) => (
+const CartIcon = ({ toogleCartHiddenProps, itemCount }) => (
   <div className="cart-icon">
-    <ShoppingIcon className="shopping-icon" onClick={toogleCartHiddenProps}/>
-    <span className="item-count">0</span>
+    <ShoppingIcon className="shopping-icon" onClick={toogleCartHiddenProps} />
+    <span className="item-count">{itemCount}</span>
   </div>
 );
+
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
+});
 
 const mapDispatchToProps = (dispath) => ({
   toogleCartHiddenProps: () => dispath(toggleCartHiddenAction()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
