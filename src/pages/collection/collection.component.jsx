4f69b92048d29914d5a5218e712d233cd2db8fc8
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-02-03 16:33:50 -0800

    explore how selectors work and how to make it work without selectors.

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,21 +7,25 @@ import { connect } from "react-redux";
 import { selectCollection } from "../../redux/shop/shop.selectors";
 
 const collectionPage = ({ collection }) => {
-  const { title, items } = collection;
+  const { title, items } = collection
   return (
-    <div className="collection-page">
-      <h2 className="title">{title.toUpperCase()}</h2>
-      <div className="items">
-        {items.map((item) => (
+  <div className="collection-page">
+    <h2 className="title">{title.toUpperCase()}</h2>
+    <div className="items">
+      {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div>
     </div>
-  );
-};
+  </div>
+)
+}
 
 const mapStateToProps = (state, ownProps) => ({
+  // would also work without reselect. 
+  // collection: state.shop.collections[ownProps.match.params.collectionId],
   collection: selectCollection(ownProps.match.params.collectionId)(state),
 });
 
 export default connect(mapStateToProps)(collectionPage);
+
+
