59ed02af3d5f48d24fbd3cb759a3951d4abb291c
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-02-23 13:28:05 -0800

    remove firestore config file

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,51 +4,21 @@ import { Route } from "react-router-dom";
 //import shopData from "./shop.data";
 import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
 import CollectionContainer from "../../pages/collection/collection.container";
-// import CollectionPage from "../../pages/collection/collection.component";
-// import WithSpinner from "../../components/with-spinner/with-spinner.component";
-
-// DB
-// import {
-//   firestore,
-//   convertCollectionsSnapshotToMap,
-// } from "../../firebase/firebase.utils";
 
 // Redux
 import { connect } from "react-redux";
 import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
-// import { createStructuredSelector } from "reselect";
-//Redux-thunk
-// import { selectIsCollectionsLoaded } from "../../redux/shop/shop.selectors";
-
-//const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class ShopPage extends React.Component {
-  // state = {
-  //   loading: true,
-  // };
-
-  //unsubscribeFromSnapshot = null;
 
   componentDidMount() {
-    // const { updateCollections } = this.props;
-
-    // const collectionRef = firestore.collection("collections");
-
-    // collectionRef.get().then(async (snapshot) => {
-    //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-    //   updateCollections(collectionsMap);
-    //   this.setState({ loading: false });
-    //});
-
-    //collectionRef.onSnapshot();
-
     const { fetchCollectionsStartAsync } = this.props;
     fetchCollectionsStartAsync();
   }
 
   render() {
     const { match } = this.props;
-    // const { loading } = this.state;
+
     return (
       <div className="shop-page">
         <Route
@@ -66,8 +36,6 @@ class ShopPage extends React.Component {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-  // updateCollections: (collectionsMap) =>
-  //   dispatch(updateCollections(collectionsMap)),
   fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
 });
 
